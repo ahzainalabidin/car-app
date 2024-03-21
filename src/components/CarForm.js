0c6a0500e9294dfb48989a7b242ea50b12755009
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-21 11:02:20 +0800

    Fix return object

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -23,7 +23,7 @@ function CarForm() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        dispatch(addCar(name, cost));
+        dispatch(addCar({ name, cost }));
     };
 
     return (
