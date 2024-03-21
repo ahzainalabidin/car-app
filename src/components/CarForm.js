b239d2b2604f8d7aa03f2f0541ad93aac1742bf1
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-21 10:23:29 +0800

    Add car name

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,7 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
 function CarForm() {
+
+    const dispatch = useDispatch();
+
+    const name = useSelector((state) => {
+        return state.form.name;
+    });
+
+    const handleNameChange = (event) => {
+        dispatch(changeName(event.target.value));
+    };
+
     return (
-        <div>CarForm</div>
+        <div className="car-form panel">
+
+            <h4 className="subtitle is-3">Add Car</h4>
+
+            <form>
+                <div className="field-group">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <input
+                            className="input is-expanded"
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                </div>
+            </form>
+
+        </div>
     );
+
 }
 
 export default CarForm;
