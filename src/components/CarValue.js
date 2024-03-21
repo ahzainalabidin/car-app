197fa7b34f13ef612a335214fdfc6ac5d145d22e
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-21 13:55:32 +0800

    Display car cost

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,29 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
+
+    const totalCost = useSelector(({ cars: { carList, searchTerm } }) => {
+
+        const filteredCars = carList.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        let cost = 0;
+
+        for (let car of filteredCars) {
+            cost += car.cost;
+        }
+
+        return cost;
+
+    });
+
     return (
-        <div>CarValue</div>
+        <div className="car-value">
+            Total Cost: ${totalCost}
+        </div>
     );
+
 }
 
 export default CarValue;
